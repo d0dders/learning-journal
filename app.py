456da1ed461017ae6155844d905b05a63be8f936
d0dders <dodders@outlook.com>
2020-11-27 22:38:29 +0000

    Editing of entries implemented

--- app.py
+++ app.py
@@ -40,6 +40,23 @@ def new():
     return render_template('new.html', form=form)
 
 
+@app.route('/entries/<int:entry_id>/edit', methods=('GET', 'POST'))
+def edit(entry_id):
+    form = forms.EntryForm()
+    if form.validate_on_submit():
+        models.Entry.update(
+            title=form.title.data.strip(),
+            created_date=form.created_date.data,
+            time_spent=form.time_spent.data.strip(),
+            learned=form.learned.data.strip(),
+            resources=form.resources.data.strip()
+        ).where(models.Entry.id == entry_id).execute()
+        return redirect(url_for('index'))
+    entry = models.Entry.get_by_id(entry_id)
+    form = forms.EntryForm(obj=entry)
+    return render_template('edit.html', form=form, entry=entry)
+
+
 @app.route('/entries/<int:entry_id>')
 def detail(entry_id):
     entry = models.Entry.get_by_id(entry_id)
